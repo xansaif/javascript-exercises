7c32342cc4cafa191d823bd2a553e5a834041e11
xan <xansaif162@gmail.com>
2023-07-01 23:53:40 +0500

    Solve exersise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+     let strAry = str.split("");
+     let revAry = strAry.reverse();
+     let revStr = revAry.join("");
+
+    return revStr;
 
 };
 
+reverseString("")
 // Do not edit below this line
 module.exports = reverseString;
