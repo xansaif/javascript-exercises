0450bf6cb7bef54c88379861fa9a8e58b36bd6b9
xan <xansaif162@gmail.com>
2023-07-01 23:38:57 +0500

    Solve exersise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(s,num) {
+    s = "";
+    for(let i = 0;i<=num;i++){
+        if(num < 0){
+            return "ERROR"
+        }
 
+        else return s+=s;
+    }
 };
-
+const number = Math.floor(Math.random() * 1000)
+repeatString("", number)
 // Do not edit below this line
 module.exports = repeatString;
