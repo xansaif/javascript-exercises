9daa33e6e3a377a6b7d702056a4660ac40f63e9d
xan <xansaif162@gmail.com>
2023-07-03 00:25:59 +0500

    Solve exersise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+  let result = ((temp - 32)*5)/9 
+  let roundR = result.toFixed(1);
+  return parseFloat(roundR);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp*(9/5))+32
+  let roundR = result.toFixed(1);
+  return parseFloat(roundR);
+
 };
 
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
