03f101352de64b610a2721191864a3b5098b1a8c
xan <xansaif162@gmail.com>
2023-07-02 23:49:27 +0500

    Solve exersise 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(num,num2) {
+    if(typeof(num)!="number" || typeof(num2)!="number")
+    {
+        return "ERROR"
+    }
+    
+    if(num > num2){
+        let sum1 = (num*(num+1)/2);
+        let sum2 = (num2*((num2+1))/2);
+        let totalSum = (sum1+1) - (sum2);       
+        return totalSum;
+    }
+    
+    else 
+    {
+    let sum1 = (num*(num+1)/2);
+    let sum2 = (num2*((num2+1))/2);
+    let totalSum = (sum2+1) - (sum1);
+    return totalSum;
+    }
+    
 };
-
+sumAll(10,[90,1]);
 // Do not edit below this line
 module.exports = sumAll;
+
+//I dont think the way I solved it was intended or is efficent buy it works  */
+
+//edit: oh shit it was the intend method
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapMassage = "";
+    if (year%4!=0)
+    {
+        leapMassage = "false"
+    }
+    else if (year%4==0)
+    {
+        if (year%100==0)
+        {
+            if (year%400==0)
+            {
+                leapMassage = "true";
+            }
+            else leapMassage = "false";
+        }
 
+        else leapMassage = "true";
+    }
+
+    return leapMassage;
 };
 
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
