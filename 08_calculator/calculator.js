36c878a61242eef7ba7b2cddd6b6deb21319847b
xan <xansaif162@gmail.com>
2023-07-31 02:33:48 +0500

    Complete ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumof = arr.reduce((sum, current) => sum + current, 0)
+  return sumof;
 };
 
-const multiply = function() {
-
+const multiply = function(...num) {
+  let multui = num.reduce((a,b) => a*b)
+ return multui;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  if(num == 0 || num == 1){ return fact}
+  else 
+  {
+    for(let i = num; i>1 ; i--){
+    fact = fact * i;
+    }
+    return fact
+  }
 };
 
 // Do not edit below this line
