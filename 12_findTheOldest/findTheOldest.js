3159d994e26aae2be2efa9188f32b2582c87b13f
xan <xansaif162@gmail.com>
2023-08-10 22:31:44 +0500

    Complete ex 12 I looked at the solution because what I was trying wasnt working and its too hot my brain wasnt working

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let newPeople = people.reduce((current, oldest) =>
+    {
+        currentAge = getAge(current.yearOfBirth , current.yearOfDeath)
+        oldestAge = getAge(oldest.yearOfBirth , oldest.yearOfDeath)
+
+        return oldestAge < currentAge ? current : oldest
+    }
+    )
+function getAge (birth , death)
+{
+    if(!death)
+    {
+        death = new Date().getFullYear()
+
+    }
+    return death - birth
+}
+
+return newPeople
 };
 
 // Do not edit below this line
